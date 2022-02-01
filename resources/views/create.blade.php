caa1fd3e0ede73ce19b682d73fc9228e54fc59bc
Salvatore949 <salvo.mess49@gmail.com>
2022-02-01 12:19:23 +0100

    alert insert

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.base')
 @section('main')
    <h1>Inserisci il movie</h1>
+
+   @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
  <form action="{{route('store')}}" method="POST">
 
   @method("POST")
